c4e4134873a4d25fa54a961a48c3d688889378cf
Javier Sarrio <javimoratalaz91@gmail.com>
2026-03-17 16:15:09 +0000

    Actualización de configuración y archivos de Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.javier.fisiko',
   appName: 'Fisiko',
-  webDir: 'public'
+  webDir: 'public', // se puede dejar así aunque no se use
+
+  server: {
+    url: 'https://studio--studio-7503491982-a84d8.us-central1.hosted.app',
+    cleartext: true
+  }
 };
 
 export default config;
